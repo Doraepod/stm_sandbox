30e1c9453b98c488e746c5ed9b73f9f1c0ffb902
Doraepod <sanjiii@rambler.ru>
2020-09-01 20:30:58 +0300

    Gyroscope library

--- gyro_test/main.c
+++ gyro_test/main.c
@@ -19,10 +19,6 @@ int main(void)
     systime_t time;
     time = chVTGetSystemTime();
 
-//    uint8_t wrbuf[2] = {0x20, 0x0F};
-//    int8_t readbuf[2] = {0, 0};
-//    msg_t msg = i2c_simple_write(0x6B, wrbuf, 2, 1000);
-
     while (true)
     {
         uint8_t i;
@@ -38,26 +34,6 @@ int main(void)
         lcdSendNumber(8, 0, angle[1]);
         lcdSendNumber(0, 1, angle[2]);
 
-
-//        msg_t msg = i2c_register_read( 0x6B, 0xA8, readbuf, 2, 1000 );
-//        if ( msg == MSG_OK )
-//        {
-//            lcdSendNumber( 0, 0, (readbuf[0] | (readbuf[1] << 8)) );
-//        }
-//        else
-//        {
-//            lcdSendString(0, 0, "Nope");
-//        }
-//        lcdSendChar( 15, 1, '!' );
-//        lcdSendString( 0, 0, "Big brother" );
-//
-//        lcdSendNumber( 12, 0, number );
-//        lcdSendString( 0, 1, "is watching you" );
-//        if( msg == MSG_OK ) // just to be sure that the address of LCD is correct
-//            {
-//                palToggleLine( LINE_LED3 );
-//            }
-//
         chThdSleepUntil(time += MS2ST(200));
 
     }
